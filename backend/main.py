e5c0fb2cdba46e37365085c57d3564d52cc076de
Rik <manmaiti7@gmail.com>
2026-09-14 23:55:42 +0530

    Fix Render dynamic port binding

--- backend/main.py
+++ backend/main.py
@@ -91,4 +91,9 @@ async def get_antibiotics_database():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )
